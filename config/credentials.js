be558f80adf8f1d82b342253e2ce78d1766fd8d6
Ian Rosner <irosner1@gmail.com>
2025-03-18 18:24:57 -0400

    Add credentials configuration

---
+++ config/credentials.js
@@ -0,0 +1,33 @@
+/**
+ * User credentials for the InstantIEP workflow demonstration
+ */
+
+module.exports = {
+  // Case Manager (ELA Teacher)
+  caseManager: {
+    email: 'irosner1@gmail.com',
+    password: 'ThisIsNotMyRealPassword$%^654',
+    name: 'Ian Rosner'
+  },
+  
+  // Math Teacher
+  mathTeacher: {
+    email: 'AEinstein@gmail.com',
+    password: '[E=mc^2]',
+    name: 'Albert Einstein'
+  },
+  
+  // Parent
+  parent: {
+    email: 'JKent51@gmail.com',
+    password: 'Super^Dad1978',
+    name: 'Jonathan Kent'
+  },
+  
+  // Student
+  student: {
+    email: 'Superman@gmail.com',
+    password: 'IAmSuperman100%',
+    name: 'Clark Kent'
+  }
+};
